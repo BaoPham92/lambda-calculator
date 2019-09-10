44755ebaae36b7afd451063ebee144a79f2feb64
Cuza <baopham92@yahoo.com>
2019-09-10 14:00:36 -0400

    Numbers data is connected

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
-};
+export const NumberButton = ({
+  number
+}) => (
+  <button>{number}</button>
+);
